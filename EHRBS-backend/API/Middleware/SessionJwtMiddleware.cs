2802b4d032616f6a4b42c7ca37d9d7b1a54e2cf9
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-09 00:19:12 +0800

    Add session

---
+++ EHRBS-backend/API/Middleware/SessionJwtMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHRBS_backend.API.Middleware
+{
+    public class SessionJwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public SessionJwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Session.GetString("JwtToken");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+
+                try
+                {
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = true,
+                        ValidIssuer = _configuration["Jwt:Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = _configuration["Jwt:Audience"],
+                        ValidateLifetime = true
+                    }, out _);
+
+                    context.Items["User"] = tokenHandler.ReadJwtToken(token);
+                }
+                catch
+                {
+                    // Invalid token
+                    context.Session.Remove("JwtToken");
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
