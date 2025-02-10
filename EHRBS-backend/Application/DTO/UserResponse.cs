34ad512417c1340a6d0f27afdd2479ecdb96708d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-10 22:17:50 +0800

    Update primary key type

--- EHRBS-backend/Application/DTO/UserResponse.cs
+++ EHRBS-backend/Application/DTO/UserResponse.cs
@@ -2,7 +2,7 @@
 {
     public class UserResponse
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
     }
