0dc1e18d2697676f3285120e2373029aac6eff2d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-10 22:17:58 +0800

    Update primary key type

--- EHRBS-backend/Domain/Entities/User.cs
+++ EHRBS-backend/Domain/Entities/User.cs
@@ -1,10 +1,13 @@
-﻿namespace EHRBS_backend.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EHRBS_backend.Domain.Entities;
 
 public class User
 {
-    public int Id { get; set; }
+    [Key]
+    public Guid Id { get; set; }
     public string FullName { get; set; }
     public string Email { get; set; }
-    public string PasswordHash { get; set; }  // ✅ Add this
+    public string PasswordHash { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
