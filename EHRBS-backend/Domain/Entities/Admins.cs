fe9300a97296a15a3e86f513cc229f08deb02059
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-12 22:27:55 +0800

    Added Admin domain

---
+++ EHRBS-backend/Domain/Entities/Admins.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EHRBS_backend.Domain.Entities
+{
+    public class Admins
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+    }
+
+    [Owned]
+    public class Permissions
+    {
+        //to be created
+    }
+}
