28f44d4744460ec469ddf394d69e2fded1935143
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-12 22:28:06 +0800

    Added AuditLogs domain

---
+++ EHRBS-backend/Domain/Entities/AuditLogs.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EHRBS_backend.Domain.Entities
+{
+    public class AuditLogs
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Action { get; set; }
+        public string TableAffected { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        
+    }
+}
