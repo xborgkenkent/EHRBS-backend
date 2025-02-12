e3cee790a9319dfc53d7d1c95e22e635295d1945
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-12 22:27:44 +0800

    Added Patients domain

---
+++ EHRBS-backend/Domain/Entities/Patients.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EHRBS_backend.Domain.Entities
+{
+    public class Patients
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string MedicalRecordNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string BloodType { get; set; }
+        public string EmergencyContact { get; set; }
+        public string InsuranceProvider { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("TenantId")]
+        public Tenants Tenants { get; set; }
+    }
+}
