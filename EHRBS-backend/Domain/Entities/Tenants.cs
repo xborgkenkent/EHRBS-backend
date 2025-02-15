5a54e1c3e165d84a8d928eaf0e8d134cf0091764
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-15 17:24:01 +0800

    Added Tenants entity

--- EHRBS-backend/Domain/Entities/Tenants.cs
+++ EHRBS-backend/Domain/Entities/Tenants.cs
@@ -14,7 +14,13 @@ namespace EHRBS_backend.Domain.Entities
         public string ContactEmail { get; set; }
         public DateTime CreatedAt { get; set; }
 
+        public ICollection<Users> Users { get; set; }
         public ICollection<Doctors> Doctors { get; set; } = new List<Doctors>();
         public ICollection<Patients> Patients { get; set; } = new List<Patients>();
+        public ICollection<Appointments> Appointments { get; set; } = new List<Appointments>();
+        public ICollection<MedicalRecords> MedicalRecords { get; set; } = new List<MedicalRecords>();
+        public ICollection<AuditLogs> AuditLogs { get; set; } = new List<AuditLogs>();
+        public ICollection<Billings> Billings { get; set; } = new List<Billings>();
+
     }
 }
