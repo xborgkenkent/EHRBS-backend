e9c373b05b6697e6aa21449a557cdbdf79ea39b7
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-15 17:24:25 +0800

    Added BillingStatus enum

---
+++ EHRBS-backend/Domain/Enums/BillingStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace EHRBS_backend.Domain.Enums
+{
+    public enum BillingStatus
+    {
+        Pending = 1,
+        Paid = 2,
+        Failed = 3,
+        Refunded = 4,
+        Cancelled = 5
+    }
+}
