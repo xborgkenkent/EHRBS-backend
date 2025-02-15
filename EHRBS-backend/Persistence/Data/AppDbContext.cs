e10fcd8a3476584732fd66a90f13b224693cb8a3
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-15 17:26:26 +0800

    Added the created entities for database

--- EHRBS-backend/Persistence/Data/AppDbContext.cs
+++ EHRBS-backend/Persistence/Data/AppDbContext.cs
@@ -6,11 +6,15 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<Users> Users { get; set; }
     public DbSet<Doctors> Doctors { get; set; }
     public DbSet<Tenants> Tenants { get; set; }
     public DbSet<Patients> Patients { get; set; }
     public DbSet<Messages> Messages { get; set; }
+    public DbSet<Appointments> Appointments { get; set; }
+    public DbSet<MedicalRecords> MedicalRecords { get; set; }
+    public DbSet<Billings> Billings { get; set; }
+    public DbSet<AuditLogs> AuditLogs { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
