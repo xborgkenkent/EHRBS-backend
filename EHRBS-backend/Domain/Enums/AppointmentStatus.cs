b210b572c5b12c8213e60f57570aa7e29bef3061
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-15 17:24:17 +0800

    Added AppointmentStatus enum

---
+++ EHRBS-backend/Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace EHRBS_backend.Domain.Enums
+{
+    public enum AppointmentStatus
+    {
+        Scheduled,    // Appointment is confirmed but not yet completed
+        Completed,    // Appointment has been successfully conducted
+        Canceled,     // Appointment was canceled by either party
+        Rescheduled,  // Appointment has been moved to a new time
+        NoShow,       // Patient did not show up for the appointment
+        Pending,
+    }
+}
