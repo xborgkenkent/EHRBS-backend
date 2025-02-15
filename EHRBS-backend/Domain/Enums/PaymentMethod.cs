184533a701dcb3951f7b02e84400ee69961cfc0c
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-02-15 17:24:34 +0800

    Added PaymentMethod enum

---
+++ EHRBS-backend/Domain/Enums/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿namespace EHRBS_backend.Domain.Enums
+{
+    public enum PaymentMethod
+    {
+        //Undecided
+
+        Cash = 1,
+        CreditCard = 2,
+        DebitCard = 3,
+        Insurance = 4,
+        BankTransfer = 5,
+        MobilePayment = 6, // (e.g., Apple Pay, Google Pay)
+        OnlinePayment = 7, // (e.g., PayPal, Stripe)
+        Other = 99 // (For any other custom method)
+    }
+}
